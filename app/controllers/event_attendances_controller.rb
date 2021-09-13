c921f6b7da8fa17d19a916615a34613d4a482d0b
BigWizards <95jsjones@gmail.com>
2021-09-13 15:07:14 +0100

    Create EventAttendance through table

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,2 +1,18 @@
 class EventAttendancesController < ApplicationController
+
+  def index
+    @event_attendances = EventAttendance.all
+  end
+
+  def new
+    @event_attendance = EventAttendance.new
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    current_user.attended_events << event
+    flash[:notice] = "Now attending this event"
+    redirect_to event
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :created_events, class_name: "Event", foreign_key: "creator_id"
   has_many :event_attendances, foreign_key: "attendee_id"
-  has_many :attended_event, through: :event_attendances
+  has_many :attended_events, class_name: "Event", through: :event_attendances
 end
