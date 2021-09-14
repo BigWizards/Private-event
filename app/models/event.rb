944ac6e2169a2182c098518915ae462a1ae324c1
BigWizards <95jsjones@gmail.com>
2021-09-14 17:10:08 +0100

    Add scopes to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: "attended_event_id"
   has_many :attendees, class_name: "User", through: :event_attendances, source: :attendee
+
+  scope :past, -> { where("date < ?", DateTime.now) }
+  scope :upcoming, -> { where("date >= ?", DateTime.now) }
 end
